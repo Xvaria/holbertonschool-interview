522dce8e84bc627ebfae4f4151211da5e4880326
Xvaria <Xvongola23@gmail.com>
2021-08-12 20:05:00 -0500

    Ready | Minimum Operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    '''Minimum Operations'''
+    operations = 0
+    div = 2
+    while(n >= div):
+        while((n % div) != 0):
+            div += 1
+        operations += div
+        n /= div
+    return(operations)
